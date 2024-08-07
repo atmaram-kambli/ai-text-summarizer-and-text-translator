214057a34160843596abad40515a16f20be5757d
Atmaram Kambli <engr.atmaram.kambli22@gmail.com>
2024-08-07 09:58:23 +0530

    added controlled component features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [textToSummarize, setTextToSummarize] = useState("");
+  const [textToTranslate, setTextToTranslate] = useState("");
+  
+  const [language, setLanguage] = useState("Hindi")
+
+  const handleOnSubmitSummarize = (e) => {
+    e.preventDefault();
+    console.log(textToSummarize)
+  }
+  
+  const handleOnSubmitTranlate = (e) => {
+    e.preventDefault();
+    console.log(textToTranslate)
+  }
+
   return (
     <>
       <h1 className='text-4xl md:text-5xl font-bold mb-4'>AI Text Summarizer And Text Translator App</h1>
@@ -9,18 +24,24 @@ function App() {
       
       <div className="container flex gap-10 flex-wrap justify-center mx-0 my-auto p-5">
         <div className="text-box w-96 rounded flex flex-col p-5 bg-white shadow-inner">
-          <textarea id="text_to_summarize" className='h-80 resize-none border-none p-2 mb-2 text-cyan-950' name="text_to_summarize" placeholder="Paste in some text to summarize. (min/max - length 200/100,000 character.)" maxlength="100000"></textarea>
-          <button id="submit-button" className="rounded bg-cyan-700 py-2 px-4">
+          <textarea id="text_to_summarize" className='h-80 resize-none border-none p-2 mb-2 text-cyan-950' name="text_to_summarize" value={textToSummarize} onChange={(e) => setTextToSummarize(e.target.value)} placeholder="Paste in some text to summarize. (min/max - length 200/100,000 character.)" maxLength="100000"></textarea>
+          <button id="submit-button" onClick={handleOnSubmitSummarize} className="rounded bg-cyan-700 py-2 px-4">
             <span className="submit-button-text">Summarize</span>
           </button>
         </div>
         <div className="text-box w-96 rounded flex flex-col p-5 bg-white shadow-inner">
-          <textarea id="summary" className='h-72 resize-none border-none p-2 mb-2 text-cyan-950' name="summarized_text" placeholder="Summarized text will appear here"></textarea>
+          <textarea id="summary" value={textToTranslate} onChange={(e) => setTextToTranslate(e.target.value)} className='h-72 resize-none border-none p-2 mb-2 text-cyan-950' name="summarized_text" placeholder="Summarized text will appear here"></textarea>
           <button id="submit-button" className="rounded bg-blue-100 py-1 mb-1 text-cyan-600">
-            <span className="submit-button-text">Hindi</span>
-            <span className='font-bold'> +</span>
+            {/* <span className="submit-button-text">Hindi</span> */}
+            <select name="language" value={language} onChange={(e) => setLanguage(e.target.value)} className='bg-inherit'>
+              <option value="Hindi">Hindi</option>
+              <option value="Marathi">Marathi</option>
+              <option value="Mandarin">Mandarin</option>
+              <option value="French">French</option>
+            </select>
+            {/* <span className='font-bold'> +</span> */}
           </button>
-          <button id="submit-button" className="rounded bg-cyan-700 py-2 px-4">
+          <button id="submit-button" onClick={handleOnSubmitTranlate} className="rounded bg-cyan-700 py-2 px-4">
             <span className="submit-button-text">Translate</span>
           </button>
         </div>
