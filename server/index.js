52e14dd4599dc54a1dd78b1e98024f1c4d9ed8a7
Atmaram Kambli <engr.atmaram.kambli22@gmail.com>
2024-08-07 12:36:08 +0530

    added the server code for summarizing the text

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const summarizeText = require('./services/summarize');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// Routes
+// 1. API ENDPOINT to summarize the text
+app.post('/summarize', async(req, res) => {
+    // console.log(req.body)
+    const summarizedText = await summarizeText(req.body.inputs);
+    console.log(summarizedText)
+    res.send(JSON.stringify(summarizedText));
+})
+
+// 2. API ENDPOINT to translate the summarized text to the selected language
+app.post('/translate', (req, res) => {
+    console.log("Translate")
+    res.send("Translate")
+})
+
+app.listen(PORT, () => {
+    console.log("App is listening on the port", PORT)
+})
