faf73c40c6b3f32063ffe1a56395e2dab7e5a8ce
Atmaram Kambli <engr.atmaram.kambli22@gmail.com>
2024-08-08 11:44:07 +0530

    makes it better✨

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,8 +9,12 @@ function App() {
   
   const [language, setLanguage] = useState("hi")
 
+  const [loading, setLoading] = useState("Summarize");
+  const [loadingT, setLoadingT] = useState("Translate");
+
   const handleOnSubmitSummarize = async(e) => {
     e.preventDefault();
+    setLoading("Loading..");
     try {
     let config = {
       method: "post",
@@ -23,9 +27,11 @@ function App() {
     }
       let response = await axios.request(config);
       // console.log(response.data);
+      setLoading("Summarize");
       setTextToTranslate(response.data);
       setSummarizedText(response.data)
     } catch (error) {
+      setLoading("Summarize");
       console.error('Error occurred:', error.message);
       console.error('Error response:', error.response);
     }
@@ -33,6 +39,7 @@ function App() {
   
   const handleOnSubmitTranlate = async(e) => {
     e.preventDefault();
+    setLoadingT("Loading..");
     try {
     let config = {
       method: "post",
@@ -44,8 +51,10 @@ function App() {
     }
       let response = await axios.request(config);
       // console.log(response.data);
+      setLoadingT("Translate")
       setTextToTranslate(response.data);
     } catch (error) {
+      setLoadingT("Translate")
       console.error('Error occurred:', error.message);
       console.error('Error response:', error.response);
     }
@@ -63,15 +72,17 @@ function App() {
       <div className="container flex gap-10 flex-wrap justify-center mx-0 my-auto p-5">
         <div className="text-box w-96 rounded flex flex-col p-5 bg-white shadow-inner">
           <textarea id="text_to_summarize" className='h-80 resize-none border-none p-2 mb-2 text-cyan-950' name="text_to_summarize" value={textToSummarize} onChange={(e) => setTextToSummarize(e.target.value)} placeholder="Paste in some text to summarize. (min/max - length 200/100,000 character.)" maxLength="100000"></textarea>
+
           <button id="submit-button" onClick={handleOnSubmitSummarize} className="rounded bg-cyan-700 py-2 px-4">
-            <span className="submit-button-text">Summarize</span>
+            <span className="submit-button-text">{loading}</span>
           </button>
+
         </div>
         <div className="text-box w-96 rounded flex flex-col p-5 bg-white shadow-inner">
           <textarea id="summary" value={textToTranslate} onChange={(e) => setTextToTranslate(e.target.value)} className='h-72 resize-none border-none p-2 mb-2 text-cyan-950' name="summarized_text" placeholder="Summarized text will appear here"></textarea>
           <button id="submit-button" className="rounded bg-blue-100 py-1 mb-1 text-cyan-600">
             {/* <span className="submit-button-text">Hindi</span> */}
-            <select name="language" value={language} onChange={handleOnChangeLanguage} className='bg-inherit'>
+            <select name="language" value={language} onChange={handleOnChangeLanguage} className='bg-inherit' disabled={textToTranslate===""}>
               <option value="hi">Hindi</option>
               <option value="fr">French</option>
               <option value="ru">Russian</option>
@@ -79,8 +90,8 @@ function App() {
             </select>
             {/* <span className='font-bold'> +</span> */}
           </button>
-          <button id="submit-button" onClick={handleOnSubmitTranlate} className="rounded bg-cyan-700 py-2 px-4" >
-            <span className="submit-button-text">Translate</span>
+          <button id="submit-button" onClick={handleOnSubmitTranlate} className="rounded bg-cyan-700 py-2 px-4" disabled={textToTranslate===""}>
+            <span className="submit-button-text">{loadingT}</span>
           </button>
         </div>
       </div>
